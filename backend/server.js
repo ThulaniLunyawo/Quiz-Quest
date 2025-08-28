7570e7b3a33439624329c68dbcb5b151e392ee9e
ezile <221321101@mycput.ac.za>
2025-08-28 19:46:46 +0200

    Fix: remove deprecated mongoose options

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,7 @@ app.use(cors()); // Allow requests from Vue app
 app.use(express.json()); // Parse JSON data
 
 // Step 4: Connect to MongoDB Atlas (we'll set this up next)
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 
 // Step 5: Define a Subject model (what data looks like)
 const Subject = mongoose.model('Subject', {
